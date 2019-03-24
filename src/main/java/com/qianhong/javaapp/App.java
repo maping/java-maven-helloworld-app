982c15c26b056c0814b637d79b3bf151f432d09e
Ma Ping <maping930883@hotmail.com>
2019-03-24 14:15:31 +0800

    Update App.java

--- src/main/java/com/qianhong/javaapp/App.java
+++ src/main/java/com/qianhong/javaapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 2019-03-24 02" );
+        System.out.println( "Hello World! 2019-03-24 03" );
     }
 }
