47fc811c708b7da64754af1f363709ef727ea3a6
Ma Ping <maping930883@hotmail.com>
2020-05-21 12:13:16 +0800

    Update App.java

--- src/main/java/com/qianhong/javaapp/App.java
+++ src/main/java/com/qianhong/javaapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 2019-03-24 05" );
+        System.out.println( "Hello World! 2020-05-21 01" );
     }
 }
